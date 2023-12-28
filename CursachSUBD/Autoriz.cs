20d572c79be0ab5f69fb6be2103beb54e893236b
deus0machine <aburgomeistus@yandex.ru>
2023-12-28 12:31:42 +0300

    v0.88
    Improve autoriz

--- CursachSUBD/Autoriz.cs
+++ CursachSUBD/Autoriz.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Npgsql;
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace CursachSUBD
@@ -6,6 +8,10 @@ namespace CursachSUBD
     public partial class Autoriz : Form
     {
         string login = "";
+        string connstring = String.Format("Server={0};Port={1};" + "User Id ={2};Password={3};Database={4}", "localhost", 5432, "postgres", "18201905", "systemBooking");
+        NpgsqlConnection conn;
+        string sql;
+        NpgsqlCommand cmd;
         public Autoriz()
         {
             InitializeComponent();
@@ -13,24 +19,35 @@ namespace CursachSUBD
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "postgres" && textBox2.Text == "admin")
+            login = textBox1.Text;
+            string password = textBox2.Text;
+            conn = new NpgsqlConnection(connstring);
+            conn.Open();
+            sql = $@"SELECT login, pass FROM users WHERE login = '{login}' AND pass = '{password}';";
+            cmd = new NpgsqlCommand(sql, conn);
+            if (cmd.ExecuteScalar()!= null)
             {
-                this.Hide();
-                login = "admin";
-                Form1 form = new Form1(login);
-                form.ShowDialog();
-            }
-            else if (textBox1.Text == "user" && textBox2.Text == "user")
-            {
-                this.Hide();
-                login = "user";
-                Form1 form = new Form1(login);
-                form.ShowDialog();
+
+                if (textBox1.Text == "postgres")
+                {
+                    this.Hide();
+                    login = "admin";
+                    Form1 form = new Form1(login);
+                    form.ShowDialog();
+                }
+                else if (textBox1.Text == "user" )
+                {
+                    this.Hide();
+                    login = "user";
+                    Form1 form = new Form1(login);
+                    form.ShowDialog();
+                }
             }
             else
             {
                 MessageBox.Show("Неверный логин или пароль!");
             }
+            conn.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
